deb807dba3dd1cdd902f1d64543cd17844427e0f
andriy-horen <go.andriy@gmail.com>
2019-10-09 23:05:35 +0200

    first problem

---
+++ src/5-longest-palindrome.test.js
@@ -0,0 +1,11 @@
+test.each([['fooanddnayo', 'anddna']])(
+    '5. Longest Palindromic Substring',
+    (input, expected) => {
+        const longestPalindrome = s => {
+            return undefined;
+        };
+
+        const result = longestPalindrome(input);
+        expect(result).toEqual(expected);
+    }
+);
