cc23ee9973e5f58c8093add656dbbe1bedfeb352
andriy-horen <go.andriy@gmail.com>
2019-10-09 23:55:33 +0200

    Longest palindrome solution

--- src/5-longest-palindrome.test.js
+++ src/5-longest-palindrome.test.js
@@ -1,11 +1,34 @@
-test.each([['fooanddnayo', 'anddna']])(
-    '5. Longest Palindromic Substring',
-    (input, expected) => {
-        const longestPalindrome = s => {
-            return undefined;
-        };
-
-        const result = longestPalindrome(input);
-        expect(result).toEqual(expected);
+function longestPalindrome(str) {
+    let longest = [0, 0];
+    for (let i = 0; i < str.length; i++) {
+        const p = findPalindrome(str, i);
+        const pLength = p[1] - p[0] + 1;
+        const longestLength = longest[1] - longest[0] + 1;
+        if (pLength > longestLength) {
+            longest = p;
+        }
     }
-);
+
+    return str.substring(longest[0], longest[1] + 1);
+}
+
+function findPalindrome(str, index) {
+    let i = index;
+    let j = index;
+
+    while (str[i] === str[j + 1]) {
+        j++;
+    }
+
+    while (i > 0 && j < str.length - 1 && str[i - 1] === str[j + 1]) {
+        i--;
+        j++;
+    }
+
+    return [i, j];
+}
+
+test.each([['fooanddnayo', 'anddna']])('5. Longest Palindromic Substring', (input, expected) => {
+    const result = longestPalindrome(input);
+    expect(result).toEqual(expected);
+});
